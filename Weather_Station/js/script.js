09eb8f571124e14aa4150c44165af244ec34bc94
LorenzoSiciliano <lorenzo.siciliano@edu.itspiemonte.it>
2017-12-12 22:47:51 +0100

    more update

--- Weather_Station/js/script.js
+++ Weather_Station/js/script.js
@@ -2,7 +2,6 @@
   //indicates if the update is paused or not
   var isStopped = false;
   var timeoutId = 0;
-  var allStationsNews = {};
   var date = new Date();
   $("#pauseUpdate").prop('disabled', true);
   $("#time").text(date.toUTCString());
@@ -13,66 +12,7 @@
   })
   .done(function(response){
     $("#pauseUpdate").prop('disabled', false);
-    $("#loading").hide();
-    allStationsNews = response;
-    console.log(allStationsNews);
-      for (var i = 0;i < allStationsNews.length; i++) {
-        console.log(allStationsNews[i]);
-        var $newAccordion = $("<div>");
-        $newAccordion.addClass("accordion");
-        $newAccordion.text(allStationsNews[i].station.name);
-        $newFlag = $("<img>");
-        $newFlag.addClass("flag");
-        $newAccordion.append($newFlag);
-        switch (allStationsNews[i].station.nation.name) {
-          case "Italia":  $newFlag.attr("src","img/italy.png");
-                          break;
-          case "Francia": $newFlag.attr("src","img/france.png");
-                          break;
-          case "Svizzera":$newFlag.attr("src","img/switzerland.png");
-                          break;
-        }
-        $("body").append($newAccordion);
-        $newAccordion.click(function(){
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            var $panel = $(panel);
-            $panel.stop();
-            if($panel.hasClass("open") == true){
-                $panel.removeClass("open");
-                $panel.slideUp();
-            }  else {
-                $panel.addClass("open");
-                $panel.slideToggle();
-              }
-        });
-        var $newStation = $("<div>");
-        $newStation.addClass("panel");
-        $("body").append($newStation);
-        var $stationFigure = $("<figure>");
-        $stationFigure.addClass("backimg");
-        $newStation.append($stationFigure);
-        $stationImg = $("<img>");
-        $stationImg.attr("src",(allStationsNews[i].station.webcam != "" ? allStationsNews[i].station.webcam : allStationsNews[i].station.image_url));
-        $stationFigure.append($($stationImg));
-        var colorFigcaption = "";
-        if (allStationsNews[i].temperature > 10) {
-            colorFigcaption = "red";
-        }
-        else {
-          colorFigcaption = "#90b4ed";
-        }
-        $stationFigure.append($("<figcaption>").text(allStationsNews[i].temperature + " °C ")
-                                                .css("color",colorFigcaption)
-                                            .append($("<img>").attr("src",(allStationsNews[i].weather_icon != null ? allStationsNews[i].weather_icon.icon : ""))));
-
-        var $stationInformation = $("<div>");
-        $stationInformation.addClass("figcap");
-        $newStation.append($stationInformation);
-        $newStation.append($("<div>").html("City: " + allStationsNews[i].station.city+"<br>Province : " + allStationsNews[i].station.province.name + "<br>Region : " +allStationsNews[i].station.region.name + "<br>Nation : "+allStationsNews[i].station.nation.name)
-                                      .addClass("nationInformation"));
-
-    }
+    setInformation(response);
   })
   .then(function(){
       timerId = setTimeout(update,10000);
@@ -84,65 +24,7 @@
       data: "json"
     })
     .done(function(response){
-      $("#loading").hide();
-      allStationsNews = response;
-      console.log(allStationsNews);
-        for (var i = 0;i < allStationsNews.length; i++) {
-          console.log(allStationsNews[i]);
-          var $newAccordion = $("<div>");
-          $newAccordion.addClass("accordion");
-          $newAccordion.text(allStationsNews[i].station.name);
-          $newFlag = $("<img>");
-          $newFlag.addClass("flag");
-          $newAccordion.append($newFlag);
-          switch (allStationsNews[i].station.nation.name) {
-            case "Italia":  $newFlag.attr("src","img/italy.png");
-                            break;
-            case "Francia": $newFlag.attr("src","img/france.png");
-                            break;
-            case "Svizzera":$newFlag.attr("src","img/switzerland.png");
-                            break;
-          }
-          $("body").append($newAccordion);
-          $newAccordion.click(function(){
-              this.classList.toggle("active");
-              var panel = this.nextElementSibling;
-              var $panel = $(panel);
-              $panel.stop();
-              if($panel.hasClass("open") == true){
-                  $panel.removeClass("open");
-                  $panel.slideUp();
-              }  else {
-                  $panel.addClass("open");
-                  $panel.slideToggle();
-                }
-          });
-          var $newStation = $("<div>");
-          $newStation.addClass("panel");
-          $("body").append($newStation);
-          var $stationFigure = $("<figure>");
-          $stationFigure.addClass("backimg");
-          $newStation.append($stationFigure);
-          $stationImg = $("<img>");
-          $stationImg.attr("src",(allStationsNews[i].station.webcam != "" ? allStationsNews[i].station.webcam : allStationsNews[i].station.image_url));
-          $stationFigure.append($($stationImg));
-          var colorFigcaption = "";
-          if (allStationsNews[i].temperature > 10) {
-              colorFigcaption = "red";
-          }
-          else {
-            colorFigcaption = "#90b4ed";
-          }
-          $stationFigure.append($("<figcaption>").text(allStationsNews[i].temperature + " °C ")
-                                                  .css("color",colorFigcaption)
-                                              .append($("<img>").attr("src",(allStationsNews[i].weather_icon != null ? allStationsNews[i].weather_icon.icon : ""))));
-
-          var $stationInformation = $("<div>");
-          $stationInformation.addClass("figcap");
-          $newStation.append($stationInformation);
-          $newStation.append($("<div>").html("City: " + allStationsNews[i].station.city+"<br>Province : " + allStationsNews[i].station.province.name + "<br>Region : " +allStationsNews[i].station.region.name + "<br>Nation : "+allStationsNews[i].station.nation.name)
-                                        .addClass("nationInformation"));
-      }
+      setInformation(response);
     })
     .fail(function(jqXHR, textStatus){
        alert('Request failed: ' + textStatus);
@@ -160,6 +42,68 @@
     });*/
   });
 });
+
+function setInformation(information){
+  $("#loading").hide();
+  console.log(information);
+    for (var i = 0;i < information.length; i++) {
+      console.log(information[i]);
+      var $newAccordion = $("<div>");
+      $newAccordion.addClass("accordion");
+      $newAccordion.text(information[i].station.name);
+      $newFlag = $("<img>");
+      $newFlag.addClass("flag");
+      $newAccordion.append($newFlag);
+      switch (information[i].station.nation.name) {
+        case "Italia":  $newFlag.attr("src","img/italy.png");
+                        break;
+        case "Francia": $newFlag.attr("src","img/france.png");
+                        break;
+        case "Svizzera":$newFlag.attr("src","img/switzerland.png");
+                        break;
+      }
+      $("body").append($newAccordion);
+      $newAccordion.click(function(){
+          this.classList.toggle("active");
+          var panel = this.nextElementSibling;
+          var $panel = $(panel);
+          $panel.stop();
+          if($panel.hasClass("open") == true){
+              $panel.removeClass("open");
+              $panel.slideUp();
+          }  else {
+              $panel.addClass("open");
+              $panel.slideToggle();
+            }
+      });
+      var $newStation = $("<div>");
+      $newStation.addClass("panel");
+      $("body").append($newStation);
+      var $stationFigure = $("<figure>");
+      $stationFigure.addClass("backimg");
+      $newStation.append($stationFigure);
+      $stationImg = $("<img>");
+      $stationImg.attr("src",(information[i].station.webcam != "" ? information[i].station.webcam : information[i].station.image_url));
+      $stationFigure.append($($stationImg));
+      var colorFigcaption = "";
+      if (information[i].temperature > 10) {
+          colorFigcaption = "red";
+      }
+      else {
+        colorFigcaption = "#90b4ed";
+      }
+      $stationFigure.append($("<figcaption>").text(information[i].temperature + " °C ")
+                                              .css("color",colorFigcaption)
+                                          .append($("<img>").attr("src",(information[i].weather_icon != null ? information[i].weather_icon.icon : ""))));
+
+      var $stationInformation = $("<div>");
+      $stationInformation.addClass("figcap");
+      $newStation.append($stationInformation);
+      $newStation.append($("<div>").html("City: " + information[i].station.city+"<br>Province : " + information[i].station.province.name + "<br>Region : " +information[i].station.region.name + "<br>Nation : "+information[i].station.nation.name)
+                                    .addClass("nationInformation"));
+  }
+}
+
 function update(){
   $.ajax({
     method: "GET",
@@ -169,22 +113,21 @@ function update(){
   .done(function(response){
     date = new Date();
     $("#time").text(date.toUTCString());
-    allStationsNews = response;
     var $accordions = $(".accordion");
     var $panelInformation = $(".panel figcaption");
     var $panelInformationIcon = $(".panel figcaption img");
     var $nationInformation = $("nationInformation");
     for (var i = 0;i < allStationsNews.length; i++) {
-      $($accordions[i]).text(allStationsNews[i].station.name);
-      $($panelInformation[i]).text(allStationsNews[i].temperature + " °C ");
+      $($accordions[i]).text(response[i].station.name);
+      $($panelInformation[i]).text(response[i].temperature + " °C ");
       if (allStationsNews[i].temperature > 10) {
           $($panelInformation[i]).css("color","red");
       }
       else {
         $($panelInformation[i]).css("color","#90b4ed");
       }
-      $($panelInformationIcon[i]).attr("src",(allStationsNews[i].weather_icon != null ? allStationsNews[i].weather_icon.icon : ""));
-      $nationInformation.html("City: " + allStationsNews[i].station.city+"<br>Province : " + allStationsNews[i].station.province.name + "<br>Region : " +allStationsNews[i].station.region.name + "<br>Nation : "+allStationsNews[i].station.nation.name);
+    //  $($panelInformationIcon[i]).attr("src",(allStationsNews[i].weather_icon != null ? allStationsNews[i].weather_icon.icon : ""));
+      $nationInformation.html("City: " + response[i].station.city+"<br>Province : " + response[i].station.province.name + "<br>Region : " +response[i].station.region.name + "<br>Nation : "+response[i].station.nation.name);
     }
   })
   .then(function(){
