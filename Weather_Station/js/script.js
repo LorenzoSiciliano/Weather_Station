2efe1cf332563fb5194d1efb330ef78efc42313d
Lorenzo Siciliano <l.siciliano@studenti.local>
2017-12-12 12:38:05 +0100

    update forse funzionante (non posso testare)

--- Weather_Station/js/script.js
+++ Weather_Station/js/script.js
@@ -1,5 +1,10 @@
 (function(){
+  //indicates if the update is paused or not
+  var isStopped = false;
+  var timeoutId = 0;
   var allStationsNews = {};
+  var date = new Date();
+  $("#time").text(date.toUTCString());
   $.ajax({
     method: "GET",
     url : "https://www.torinometeo.org/api/v1/realtime/data/",
@@ -30,12 +35,11 @@
             this.classList.toggle("active");
             var panel = this.nextElementSibling;
             var $panel = $(panel);
+            $panel.stop();
             if($panel.hasClass("open") == true){
-                $panel.stop();
                 $panel.removeClass("open");
                 $panel.slideUp();
             }  else {
-                $panel.stop();
                 $panel.addClass("open");
                 $panel.slideToggle();
               }
@@ -49,11 +53,23 @@
         $stationImg = $("<img>");
         $stationImg.attr("src",(allStationsNews[i].station.webcam != "" ? allStationsNews[i].station.webcam : allStationsNews[i].station.image_url));
         $stationFigure.append($($stationImg));
+        var colorFigcaption = "";
+        if (allStationsNews[i].temperature > 10) {
+            colorFigcaption = "red";
+        }
+        else {
+          colorFigcaption = "#90b4ed";
+        }
         $stationFigure.append($("<figcaption>").text(allStationsNews[i].temperature + " °C ")
+                                                .css("color",colorFigcaption)
                                             .append($("<img>").attr("src",(allStationsNews[i].weather_icon != null ? allStationsNews[i].weather_icon.icon : ""))));
+
         var $stationInformation = $("<div>");
         $stationInformation.addClass("figcap");
         $newStation.append($stationInformation);
+        $newStation.append($("<div>").html("City: " + allStationsNews[i].station.city+"<br>Province : " + allStationsNews[i].station.province.name + "<br>Region : " +allStationsNews[i].station.region.name + "<br>Nation : "+allStationsNews[i].station.nation.name)
+                                      .addClass("nationInformation"));
+       refresh(10000);
     }
   })
   .fail(function(jqXHR, textStatus){
@@ -71,5 +87,51 @@
     });*/
   });
 });
+function update(){
+  $.ajax({
+    method: "GET",
+    url : "https://www.torinometeo.org/api/v1/realtime/data/",
+    data: "json"
+  })
+  .done(function(response){
+    date = new Date();
+    $("#time").text(date.toUTCString());
+    allStationsNews = response;
+    var $accordions = $(".accordion");
+    var $panelInformation = $(".panel figcaption");
+    var $panelInformationIcon = $(".panel figcaption img");
+    var $nationInformation = $("nationInformation");
+    for (var i = 0;i < allStationsNews.length; i++) {
+      $($accordions[i]).text(allStationsNews[i].station.name);
+      $($panelInformation[i]).text(allStationsNews[i].temperature + " °C ");
+      if (allStationsNews[i].temperature > 10) {
+          $($panelInformation[i]).css("color","red");
+      }
+      else {
+        $($panelInformation[i]).css("color","#90b4ed");
+      }
+      $($panelInformationIcon[i]).attr("src",(allStationsNews[i].weather_icon != null ? allStationsNews[i].weather_icon.icon : ""));
+      $nationInformation.html("City: " + allStationsNews[i].station.city+"<br>Province : " + allStationsNews[i].station.province.name + "<br>Region : " +allStationsNews[i].station.region.name + "<br>Nation : "+allStationsNews[i].station.nation.name);
+    }
+  })
+  .fail(function(jqXHR, textStatus){
+    alert('Request failed: ' + textStatus);
+  });
+}
 
+function refresh(milliseconds) {
+  timerId = setTimeout(function updateInterval(){
+    update();
+    timerId = setTimeout(updateInterval,milliseconds);
+  }, milliseconds);
+}
+
+$("#pauseUpdate").click(function(){
+    if (!isStopped) {
+      clearTimeout(timeoutId);
+    }else {
+      isStopped = false;
+      refresh(10000);
+    }
+});
 })()
